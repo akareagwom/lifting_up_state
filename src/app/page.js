33ea0d7f4a710f5bb1fa8f749acc517585f7d63c
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-06-08 12:41:40 +0100

    state declared at chid component

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,9 @@
-// import {Discuss} from "@/components/discuss";
-"use client";
-import {useState} from "react"
-
-function Panel ({title, children}){
-   const [active, setActive] = useState(false);
-   return(
-    <section>
-      <h3>{title}</h3>
-    </section>
-   )
-}
+import Discuss from "../components/discuss"
 
 export default function Home() {
-  const [count, setCount] = useState("")
   return (
-   <div className="bg-red-400">
-    {/* <Discuss/> */}
-    hello
+   <div className="bg-[#928490] h-screen">
+    <Discuss/>
    </div>
   )
 }
--- src/components/discuss.jsx
+++ src/components/discuss.jsx
@@ -1,12 +1,33 @@
-// "use client";
-// import {useState} from 'react';
+"use client";
+import {useState} from "react"
 
+function Panel ({title, children}){
+   const [active, setActive] = useState(false);
+   return(
+    <section>
+      <h3>{title}</h3>
+      {active? (
+        <p>{children}</p>
+      ):(
+        <button className="bg-[#928490]" onClick={() => setActive(true)}>SHOW</button>
+      )}
+    </section>
+   )
+}
 
 export default function Discuss() {
   return (
-   <div className="bg-red-400">
-    <h1>Lifting Up State</h1>
-    
+   <div className="">
+    <h3 className="text-3xl text-white">state in the child component</h3>
+    <div className=" bg-[#433E49] flex justify-around w-[40%]">
+    <Panel  title="women" >
+      Did You Know That The First Computer Programmer Was a Woman
+    </Panel>
+    <Panel title="men" >
+      Did You Know That The Father of Computer Was a Man
+    </Panel>
+    </div>
+    hello
    </div>
   )
 }
