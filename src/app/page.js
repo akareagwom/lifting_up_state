5390d2d5af05615c308ca0dd2dc0053619c2a012
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-06-08 13:58:11 +0100

    parent component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Talk from "@/components/talk"
 import Discuss from "../components/discuss"
 
 export default function Home() {
   return (
    <div className="bg-[#928490] h-screen">
     <Discuss/>
+    <Talk/>
    </div>
   )
 }
---
+++ src/components/talk.jsx
@@ -0,0 +1,10 @@
+"use client";
+import {useState} from "react"
+
+export default function Talk (){
+    return (
+        <div className="bg-[#DBC1AD] w-[40%]">
+ggggg
+        </div>
+    )
+}
