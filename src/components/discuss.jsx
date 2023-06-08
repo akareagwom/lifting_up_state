05297dbb010311910e4b5681da0d6d6f921000d9
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-06-08 08:51:28 +0100

    use client for server component

---
+++ src/components/discuss.jsx
@@ -0,0 +1,12 @@
+// "use client";
+// import {useState} from 'react';
+
+
+export default function Discuss() {
+  return (
+   <div className="bg-red-400">
+    <h1>Lifting Up State</h1>
+    
+   </div>
+  )
+}
