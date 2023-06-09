13487568d25cbfb696b2b8f1d8534ffb1568ddd3
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-06-09 08:00:49 +0100

    state declared at parent component

--- src/components/talk.jsx
+++ src/components/talk.jsx
@@ -6,7 +6,30 @@ export default function Talk (){
     return (
         <div className="bg-[#DBC1AD] w-[40%]">
             <h2>I am a STATE in a PARENT component.</h2>
-            
+            <Greet
+            isActive={activeIndex === 0}
+            onShow={()=>setActiveIndex(0)}>
+                hello
+            </Greet>
+            <Greet
+            isActive={activeIndex === 1}
+            onShow={()=>setActiveIndex(1)}>
+                hello
+            </Greet>
+
         </div>
+    )
+};
+
+function Greet({title,children,isActive,onShow}){
+    return(
+        <>
+        <h3>{title}</h3>
+        {isActive?(
+            <p>{children}</p>
+        ):(
+            <button onClick={()=> onShow(true)}>show</button>
+        )}
+        </>
     )
 }
