b515449b495d6693c81d94294037f5958e4a0625
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-08-30 23:32:42 +0100

    useState

--- src/Dashboard/index.jsx
+++ src/Dashboard/index.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react"
+
 export const App =()=>{
-    
+    const [age,setAge] = useState(45)
+    const handleClick=()=>{
+        setAge(age+1);
+    }
     return(
         <>
-        <p>3</p>
+        <button className="bg-blue-400" onClick={handleClick}>{age}</button>
         </>
     )
 }
