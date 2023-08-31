1da266e97b870d235aa1b72ecd0fcc76c280c992
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-08-31 07:11:28 +0100

    useEffect

--- src/Dashboard/index.jsx
+++ src/Dashboard/index.jsx
@@ -1,12 +1,31 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { createConnection } from "./chat";
 
-export const App =()=>{
-    const [age,setAge] = useState(45)
+export const App =({roomId})=>{
+    const [age,setAge] = useState(45);
+    const [serverUrl, setServerUrl] = useState('https://localhost:1234');
+
+    useEffect(()=>{
+        const connection = createConnection(serverUrl,roomId)
+        connection.connect();
+        return () => {
+          connection.disconnect();
+        };
+    },[serverUrl,roomId])
+
     const handleClick=()=>{
         setAge(age+1);
     }
     return(
         <>
+         <label>
+        Server URL:{' '}
+        <input
+          value={serverUrl}
+          onChange={e => setServerUrl(e.target.value)}
+        />
+      </label>
+      <h1>Welcome to the {roomId} room!</h1>
         <button className="bg-blue-400" onClick={handleClick}>{age}</button>
         </>
     )
